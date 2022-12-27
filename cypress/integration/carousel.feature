8af60a7c93b8dee245ea68c88657b161451a268b
anagarcia791 <amgt2697@gmail.com>
2022-12-26 23:35:11 -0500

    assertions for next and prev carousel buttons, added

--- cypress/integration/carousel.feature
+++ cypress/integration/carousel.feature
@@ -5,7 +5,15 @@ Feature: Check carousel behaviour
 
   Background:
     Given User visit the Demoblaze homepage
-    
+
   Scenario: Check that carousel changes image by itself
     When User waits for carousel change its image
     Then Sees a diferent image from the first one
+
+  Scenario: User clicks carousel to next slide
+    When User clicks next button
+    Then Sees "second" slide
+
+  Scenario: User clicks carousel to previous slide
+    When User clicks previous button
+    Then Sees "third" slide
--- cypress/integration/carousel/carouselSteps.js
+++ cypress/integration/carousel/carouselSteps.js
@@ -21,3 +21,23 @@ Then("Sees a diferent image from the first one", () => {
       expect(srcText).to.not.equal(carouselData.firstSlideSrc);
     });
 });
+
+When("User clicks next button", () => {
+  homePage.clickCarouselNextButton();
+  cy.wait(1000);
+  homePage.getSliderActiveImage(0).as("imageShown");
+});
+
+When("User clicks previous button", () => {
+  homePage.clickCarouselPrevButton();
+  cy.wait(1000);
+  homePage.getSliderActiveImage(0).as("imageShown");
+});
+
+Then("Sees {string} slide", (slideToCheck) => {
+  cy.get("@imageShown")
+    .should("have.attr", "src")
+    .then((srcText) => {
+      expect(srcText).to.equal(carouselData[slideToCheck + "SlideSrc"]);
+    });
+});
