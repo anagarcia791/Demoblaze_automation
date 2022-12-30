c1a351aea6e19854540829d09a0bbaebbc0182bb
anagarcia791 <amgt2697@gmail.com>
2022-12-30 00:06:08 -0500

    allure report added

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,11 +1,10 @@
 /// <reference types="cypress" />
 
 const cucumber = require("cypress-cucumber-preprocessor").default;
+const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
-/**
- * @type {Cypress.PluginConfig}
- */
-// eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
   on("file:preprocessor", cucumber());
+  allureWriter(on, config);
+  return config;
 };
